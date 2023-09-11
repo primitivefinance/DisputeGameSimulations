eb86ead3f6e9267286b7ae11d2b25875c97a0671
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-09-11 17:22:52 +0200

    init
    forge install: optimism
    deployed dispute factory

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use arbiter_core::{
     environment::EnvironmentParameters, manager::Manager, middleware::RevmMiddleware,
 };
+use ethers::providers::Middleware;
 use std::{error::Error, sync::Arc};
+mod bindings;
+use crate::bindings::dispute_game_factory::dispute_game_factory::DisputeGameFactory;
 
-const TEST_ENV_LABEL: &str = "test";
+const TEST_ENV_LABEL: &str = "test_env";
 // https://github.com/ethereum-optimism/optimism/tree/develop/op-challenger
 
 #[tokio::main]
@@ -17,10 +20,19 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
             seed: 1,
         },
     );
-    let _client_with_signer = Arc::new(RevmMiddleware::new(
+    let _ = manager.start_environment(TEST_ENV_LABEL);
+    let client_with_signer = Arc::new(RevmMiddleware::new(
         manager.environments.get(TEST_ENV_LABEL).unwrap(),
         None,
     ));
+    println!("client at address {}", client_with_signer.default_sender().unwrap());
 
+    let factory = DisputeGameFactory::deploy(client_with_signer.clone(),())
+        .unwrap()
+        .send()
+        .await
+        .unwrap();
+
+    println!("Factory address: {}", factory.address());
     Ok(())
 }
