b46d315c21b5a3f0de8a990f1e031f15f68f0836
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-09-11 15:49:27 +0200

    clean for submodules

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 use arbiter_core::{
     environment::EnvironmentParameters, manager::Manager, middleware::RevmMiddleware,
 };
-use ethers::providers::Middleware;
 use std::{error::Error, sync::Arc};
-
-use crate::bindings::counter::Counter;
-
-mod bindings;
 
 const TEST_ENV_LABEL: &str = "test";
 
@@ -21,29 +16,10 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
             seed: 1,
         },
     );
-
-    let client_with_signer = Arc::new(RevmMiddleware::new(
+    let _client_with_signer = Arc::new(RevmMiddleware::new(
         manager.environments.get(TEST_ENV_LABEL).unwrap(),
         None,
     ));
-    println!(
-        "created client with address {:?}",
-        client_with_signer.default_sender().unwrap()
-    );
-    manager.start_environment(TEST_ENV_LABEL)?;
-
-    let counter = Counter::deploy(client_with_signer.clone(), ())?
-        .send()
-        .await?;
-    println!("Counter contract deployed at {:?}", counter.address());
-
-    for index in 0..10 {
-        let _ = counter.increment().send().await?.await?;
-        println!("Counter incremented to {}", index + 1);
-    }
-    // post state mutation call to show that the state has changed with send
-    let count = counter.number().call().await?;
-    println!("Counter count is {}", count);
 
     Ok(())
 }
